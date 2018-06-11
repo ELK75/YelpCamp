ee6609fea8c809c9aa8c055982759e228a67e5d2
ELK75 <elijah.kajinic@gmail.com>
2018-06-11 11:51:40 -0400

    updat routes

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.use("/", indexRoutes);
+app.use("", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
